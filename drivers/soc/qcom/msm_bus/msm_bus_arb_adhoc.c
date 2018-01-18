fabeb94d7b9f368bfd7edcd4c10c9636918e2edd
Philip Cuadra <philipcuadra@google.com>
2018-01-18 11:32:26 -0800

    msm: bus_arb: disable debug logging
    This debug logging consumes 10% of all the cpu cycles in the drivers
    communicating with the dsp.  Disable the logging for the time being
    until it we add a config.
    
    Bug: 71867957
    Change-Id: I97d418fee3d4576b077ed284ed5ae4447da5a789
    Signed-off-by: Philip Cuadra <philipcuadra@google.com>
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Yasir-Siddiqui <www.mohammad.yasir.s@gmail.com>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- drivers/soc/qcom/msm_bus/msm_bus_arb_adhoc.c
+++ drivers/soc/qcom/msm_bus/msm_bus_arb_adhoc.c
@@ -1330,8 +1330,8 @@ static int update_bw_context(struct msm_bus_client_handle *cl, u64 act_ab,
 	if (!slp_ab && !slp_ib)
 		cl->active_only = true;
 	if (DEBUG_REC_TRANSACTION)
-		msm_bus_dbg_rec_transaction(cl, cl->cur_act_ab, cl->cur_slp_ib);
-
+		msm_bus_dbg_rec_transaction(cl, cl->cur_act_ab,
+					    cl->cur_slp_ib);
 	ret = update_path(cl->mas_dev, cl->slv, act_ib, act_ab, slp_ib, slp_ab,
 				cl->cur_act_ab, cl->cur_act_ab,  cl->first_hop,
 				cl->active_only);
