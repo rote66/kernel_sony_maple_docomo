e4e9a0e2affd2268da1576b455c7a27cc6583a31
rote66 <26439391+rote66@users.noreply.github.com>
2025-01-31 00:04:39 +0800

    BACKPORT:kthread: kthread worker API cleanup continue Cleaning up api leftovers in local source code

--- kernel/sched/cpufreq_schedalucard.c
+++ kernel/sched/cpufreq_schedalucard.c
@@ -601,7 +601,7 @@ static void acgov_irq_work(struct irq_work *irq_work)
 	 * after the work_in_progress flag is cleared. The effects of that are
 	 * neglected for now.
 	 */
-	queue_kthread_work(&sg_policy->worker, &sg_policy->work);
+	kthread_queue_work(&sg_policy->worker, &sg_policy->work);
 }
 
 /************************** sysfs interface ************************/
@@ -941,8 +941,8 @@ static int acgov_kthread_create(struct acgov_policy *sg_policy)
 	if (policy->fast_switch_enabled)
 		return 0;
 
-	init_kthread_work(&sg_policy->work, acgov_work);
-	init_kthread_worker(&sg_policy->worker);
+	kthread_init_work(&sg_policy->work, acgov_work);
+	kthread_init_worker(&sg_policy->worker);
 	thread = kthread_create(kthread_worker_fn, &sg_policy->worker,
 				"acgov:%d",
 				cpumask_first(policy->related_cpus));
@@ -974,7 +974,7 @@ static void acgov_kthread_stop(struct acgov_policy *sg_policy)
 	if (sg_policy->policy->fast_switch_enabled)
 		return;
 
-	flush_kthread_worker(&sg_policy->worker);
+	kthread_flush_worker(&sg_policy->worker);
 	kthread_stop(sg_policy->thread);
 	mutex_destroy(&sg_policy->work_lock);
 }
